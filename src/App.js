c08c58c8eebdb92de97125fce93ca0d1f56212ca
dubhcait <dubhcait@live.ie>
2019-09-03 16:34:40 +0100

    add simple lambda hello function

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [loading, setLoading] = React.useState(false);
+const [msg, setMesg] = React.useState("");
+ const handleClick = api => e => {
+    e.preventDefault()
+    setLoading(true)
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json =>  {setLoading(false); setMesg(json.msg)})
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <p>  <span>{msg}</span>
+      </p>
       </header>
     </div>
   );
